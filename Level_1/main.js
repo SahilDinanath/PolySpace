176433137827c3d0b10086a92921fb7843e7fce7
Sahil Dinanath <sahildinanath@debian.local>
2023-09-29 22:06:51 +0200

    added player movement with multiple keys

--- Level_1/main.js
+++ Level_1/main.js
@@ -7,20 +7,57 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const geometry = new THREE.BoxGeometry( 1, 1, 1);
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 10;
 
 function animate() {
+	
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
+	// cube.rotation.x += 0.01;
+	// cube.rotation.y += 0.01;
+	movement(cube);
 	renderer.render( scene, camera );
+	
 }
+
+//player keyboard input for movement
+let ySpeed = 0.1;
+let xSpeed =0.1;
+let playerMoving = false;
+var keys = new Map();
+
+document.onkeydown= function (e) {
+    keys.set(e.which,true);
+};
+
+document.onkeyup = function (e) {
+    keys.delete(e.which);
+};
+
+function movement(object) {
+	keys.forEach((value, key)=> {
+	if (key == 37 && value == true){
+			object.position.x -= xSpeed;			
+		}
+		else if (key == 38 && value == true){
+
+			object.position.z -= xSpeed;			
+		}
+		else if (key == 39 && value == true){
+
+			object.position.x += xSpeed;			
+		}
+		else if (key == 40 && value == true){
+			object.position.z += xSpeed;			
+		}
+	}	
+	);
+	
+};
 
 animate();
