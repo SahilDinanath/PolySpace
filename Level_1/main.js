909fc71e33852539f4cf59bed288dd1e5288bff2
Sahil Dinanath <sahildinanath@debian.local>
2023-10-09 21:10:12 +0200

    moved player code to player.js file

--- Level_1/main.js
+++ Level_1/main.js
@@ -1,22 +1,36 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-
+import * as player from './Player/player.js';
 
 //dom access
+//start screen
 var levelOneButton = document.getElementById("level_1");
 var levelTwoButton = document.getElementById("level_2");
 var levelThreeButton = document.getElementById("level_3");
 var title = document.getElementById("title");
 
+//user input for player movement
+const keys = new Map();
 document.onkeydown = function(e) {
-	playerMoving = true;
+	player.setPlayerMoving(true);
 	keys.set(e.which, true);
 };
 
 document.onkeyup = function(e) {
-	playerMoving = false;
+	player.setPlayerMoving(false);
 	keys.delete(e.which);
 };
+
+//dom functions
+function removeStartScreen(){
+	levelOneButton.hidden = true;
+	levelTwoButton.hidden = true;
+	levelThreeButton.hidden = true;
+	title.hidden = true;
+}
+
+//
+//game below
+//
 
 const scene = new THREE.Scene();
 
@@ -34,95 +48,19 @@ const ambientLighting = new THREE.AmbientLight("white", 30);
 //object setup in world
 function worldLevelOne() {
 	scene.add(ambientLighting);
-	loadPlayer();
+	player.loadPlayer(scene);
 }
 
 //run values that are updated continously 
 function animate() {
-
-
 	requestAnimationFrame(animate);
-
 	//moves player
-	keyboardMoveObject(scene.getObjectByName("player"),keys);
+	player.keyboardMoveObject(scene.getObjectByName("player"),keys);
 	renderer.render(scene, camera);
 
 }
 
-/* Player */
-//load models
-const loader = new GLTFLoader();
-function loadPlayer() {
-	loader.load('Assets/Player/StarSparrow.glb',
-		//we have to set up player in here for now
-		function(player) {
-			player.scene.name = "player";
-			scene.add(player.scene).rotateY(Math.PI);
-		});
-}
-
-
-
-
-
-
-
-
-//player keyboard input for movement
-const speed = 0.5;
-const yMovementBounds = 16;
-const xMovementBounds = 16;
-var playerMoving = false;
-const keys = new Map();
-
-function keyboardMoveObject(object,keys) {
-	if (object == undefined)
-		return;
-	if (playerMoving) {
-		keys.forEach((_, key) => {
-			if (key == 37) {
-				object.position.x += object.position.x < xMovementBounds ? speed : 0;
-			}
-			else if (key == 38) {
-				object.position.y += object.position.y < yMovementBounds ? speed : 0;
-			}
-			else if (key == 39) {
-
-				object.position.x -= object.position.x > -xMovementBounds ? speed : 0;
-			}
-			else if (key == 40) {
-				object.position.y -= object.position.y > -yMovementBounds ? speed : 0;
-			}
-		});
-
-	}
-	else {
-		if (object.position.x != 0 || object.position.y != 0) {
-			if (object.position.x > 0) {
-				object.position.x -= speed / 2;
-			} else if (object.position.x < 0) {
-				object.position.x += speed / 2;
-			}
-			if (object.position.y > 0) {
-				object.position.y -= speed / 2;
-			} else if (object.position.y < 0) {
-				object.position.y += speed / 2;
-			}
-		}
-
-	}
-
-};
-
-function removeStartScreen(){
-	levelOneButton.hidden = true;
-	levelTwoButton.hidden = true;
-	levelThreeButton.hidden = true;
-	title.hidden = true;
-}
-
-
-//create objects
+//spawn level depending on button click 
 levelOneButton.onclick = function() {
 	removeStartScreen();
 	worldLevelOne();
