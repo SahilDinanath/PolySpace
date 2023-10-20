ed0402feaea67613fdcf228a69786274f3cc5e0b
naomi <naomineo4@gmail.com>
2023-10-21 00:47:08 +0200

    skybox textures

--- Levels/Level_2.js
+++ Levels/Level_2.js
@@ -1,2 +1,19 @@
-
+// import * as bosses from '/Bosses/bosses.js';
+// import * as obstacles from '/Obstacles/obstacles.js';
+// import * as THREE from 'three';
+// import * as player from '/Player/player_exports.js';
+// import * as ui from '/UI/ui_exports.js'
+//
+// export function levelOne(scene,renderer,camera) {
+//     //sets up lighting
+//     const ambientLighting = new THREE.AmbientLight("white", 6);
+//     scene.add(ambientLighting);
+//
+//     //sets up objects in scene
+//     player.addPlayerToScene(scene);
+//     ui.addMiniMapToScene(scene);
+//     bosses.bossTwo(camera, scene, renderer);
+//     obstacles.animateObstacles(renderer, camera, scene);
+//
+// }
 
