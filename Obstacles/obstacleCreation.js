81875829c31a8504005f35c88b39885585199044
Ilona Smith <smithilona247@gmail.com>
2023-10-22 09:52:54 +0200

    moved creation of obstacles into new file

---
+++ Obstacles/obstacleCreation.js
@@ -0,0 +1,133 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+export let obstaclesBoundingBoxes = [];
+export let obstacles = [];
+let obstacleBoundingBox;
+
+const opacityVal = 0.4;
+const transparency = true;
+const obstacleMaterial = new THREE.MeshLambertMaterial({ color: 0x800080, opacity:opacityVal, transparent:transparency, side: THREE.DoubleSide });
+
+const loader = new GLTFLoader();
+
+export function addTreeToScene(scene) {
+	loader.load('Assets/treeTextures/tree.glb',
+		function(tree) {
+			tree.scene.name = "tree";
+			tree.scene.rotateY(Math.PI);
+      tree.scene.scale.set(100, 100, 100);
+			// tree.scene.children[0].geometry.computeBoundingBox();
+			// playerBoundingBox = new THREE.Box3().setFromObject(tree.scene);
+
+      // this is to make a bounding box visible but does not work with collision detection
+			const treeBoundingBox = new THREE.BoxHelper(tree.scene, 0xff0000);
+			scene.add(treeBoundingBox);
+			
+			scene.add(tree.scene);
+		});
+}
+
+export function createObstacle(scene, MIN_Z) {
+    const randomNum = Math.floor(Math.random() * 3) + 1;
+  
+    const z = MIN_Z;
+    const x = 0;
+    const y = 0;
+    let obstacle = null;
+  
+    switch(randomNum){
+      case 1:
+        obstacle = ObstacleConfig1(x, y, z);
+        break;
+      case 2:
+        obstacle = ObstacleConfig2(x, y, z);
+        break;
+      case 3:
+        obstacle = ObstacleConfig3(x, y, z);
+        break;
+    }
+    obstacles.push(obstacle);
+    scene.add(obstacle);
+  }
+
+export function ObstacleConfig1(x, y, z) {
+    const obstacle = new THREE.Group();
+
+    const box1 = new THREE.Mesh(new THREE.BoxGeometry(15, 10, 10), obstacleMaterial);
+    obstacleBoundingBox = new THREE.Box3().setFromObject(box1);
+    obstaclesBoundingBoxes.push(obstacleBoundingBox);
+
+    const box2 = new THREE.Mesh(new THREE.BoxGeometry(15, 36, 10), obstacleMaterial);
+    obstacleBoundingBox = new THREE.Box3().setFromObject(box2);
+    obstaclesBoundingBoxes.push(obstacleBoundingBox);
+
+    const box3 = new THREE.Mesh(new THREE.BoxGeometry(15, 10, 10), obstacleMaterial);
+    obstacleBoundingBox = new THREE.Box3().setFromObject(box3);
+    obstaclesBoundingBoxes.push(obstacleBoundingBox);
+
+    box1.position.set(15, 13, 0);
+    box2.position.set(0, 0, 0);
+    box3.position.set(-15, -13, 0);
+
+    obstacle.add(box1);
+    obstacle.add(box2);
+    obstacle.add(box3);
+
+    obstacle.position.set(x, y, z);
+    return obstacle;
+}
+
+export function ObstacleConfig2(x, y, z) {
+    const obstacle = new THREE.Group();
+
+    const box1 = new THREE.Mesh(new THREE.BoxGeometry(15, 10, 10), obstacleMaterial);
+    obstacleBoundingBox = new THREE.Box3().setFromObject(box1);
+    obstaclesBoundingBoxes.push(obstacleBoundingBox);
+
+    const box2 = new THREE.Mesh(new THREE.BoxGeometry(15, 36, 10), obstacleMaterial);
+    obstacleBoundingBox = new THREE.Box3().setFromObject(box2);
+    obstaclesBoundingBoxes.push(obstacleBoundingBox);
+
+    const box3 = new THREE.Mesh(new THREE.BoxGeometry(15, 36, 10), obstacleMaterial);
+    obstacleBoundingBox = new THREE.Box3().setFromObject(box3);
+    obstaclesBoundingBoxes.push(obstacleBoundingBox);
+
+    box1.position.set(15, 0, 0);
+    box2.position.set(0, 0, 0);
+    box3.position.set(-15, 0, 0);
+
+    obstacle.add(box1);
+    obstacle.add(box2);
+    obstacle.add(box3);
+
+    obstacle.position.set(x, y, z);
+    return obstacle;
+}
+
+export function ObstacleConfig3(x, y, z) {
+    const obstacle = new THREE.Group();
+  
+    const box1 = new THREE.Mesh(new THREE.BoxGeometry(15, 24, 10), obstacleMaterial);
+    obstacleBoundingBox = new THREE.Box3().setFromObject(box1);
+    obstaclesBoundingBoxes.push(obstacleBoundingBox);
+  
+    const box2 = new THREE.Mesh(new THREE.BoxGeometry(15, 24, 10), obstacleMaterial);
+    obstacleBoundingBox = new THREE.Box3().setFromObject(box2);
+    obstaclesBoundingBoxes.push(obstacleBoundingBox);
+  
+    const box3 = new THREE.Mesh(new THREE.BoxGeometry(15, 24, 10), obstacleMaterial);
+    obstacleBoundingBox = new THREE.Box3().setFromObject(box3);
+    obstaclesBoundingBoxes.push(obstacleBoundingBox);
+  
+    box1.position.set(15, 6, 0);
+    box2.position.set(0, 6, 0);
+    box3.position.set(-15, -6, 0);
+  
+    obstacle.add(box1);
+    obstacle.add(box2);
+    obstacle.add(box3);
+  
+    obstacle.position.set(x, y, z);
+    return obstacle;
+}
--- Obstacles/obstacles.js
+++ Obstacles/obstacles.js
@@ -1,139 +1,26 @@
-import * as THREE from 'three';
 import { playerBoundingBox } from '/Player/player.js';
+import { obstacles } from '/Obstacles/obstacleCreation.js';
+import { obstaclesBoundingBoxes } from '/Obstacles/obstacleCreation.js';
+import { createObstacle } from '/Obstacles/obstacleCreation.js';
 import { onDeath } from '/Player/player.js';
 import { isPaused } from '/main.js';
 
 //where obstacle should be generated
-const MIN_Z = -350;
+const MIN_Z = -360;
 //where obstacle should be removed
 const MAX_Z = 25;
 
-const obstacles = [];
-const obstaclesBoundingBoxes = [];
-let obstacleBoundingBox;
 export let collisionDetected = false;
-
-const opacityVal = 0.4;
-const transparency = true;
-const obstacleMaterial = new THREE.MeshLambertMaterial({ color: 0x800080,opacity:opacityVal, transparent:transparency});
-
-function createObstacle1(x, y, z) {
-  const obstacle = new THREE.Group();
-
-  const box1 = new THREE.Mesh(new THREE.BoxGeometry(15, 10, 10), obstacleMaterial);
-  obstacleBoundingBox = new THREE.Box3().setFromObject(box1);
-  obstaclesBoundingBoxes.push(obstacleBoundingBox);
-
-  const box2 = new THREE.Mesh(new THREE.BoxGeometry(15, 36, 10), obstacleMaterial);
-  obstacleBoundingBox = new THREE.Box3().setFromObject(box2);
-  obstaclesBoundingBoxes.push(obstacleBoundingBox);
-
-  const box3 = new THREE.Mesh(new THREE.BoxGeometry(15, 10, 10), obstacleMaterial);
-  obstacleBoundingBox = new THREE.Box3().setFromObject(box3);
-  obstaclesBoundingBoxes.push(obstacleBoundingBox);
-
-  box1.position.set(15, 13, 0);
-  box2.position.set(0, 0, 0);
-  box3.position.set(-15, -13, 0);
-
-  obstacle.add(box1);
-  obstacle.add(box2);
-  obstacle.add(box3);
-
-  obstacle.position.set(x, y, z);
-  return obstacle;
-}
-
-function createObstacle2(x, y, z) {
-  const obstacle = new THREE.Group();
-
-  const box1 = new THREE.Mesh(new THREE.BoxGeometry(15, 10, 10), obstacleMaterial);
-  obstacleBoundingBox = new THREE.Box3().setFromObject(box1);
-  obstaclesBoundingBoxes.push(obstacleBoundingBox);
-
-  const box2 = new THREE.Mesh(new THREE.BoxGeometry(15, 36, 10), obstacleMaterial);
-  obstacleBoundingBox = new THREE.Box3().setFromObject(box2);
-  obstaclesBoundingBoxes.push(obstacleBoundingBox);
-
-  const box3 = new THREE.Mesh(new THREE.BoxGeometry(15, 36, 10), obstacleMaterial);
-  obstacleBoundingBox = new THREE.Box3().setFromObject(box3);
-  obstaclesBoundingBoxes.push(obstacleBoundingBox);
-
-  box1.position.set(15, 0, 0);
-  box2.position.set(0, 0, 0);
-  box3.position.set(-15, 0, 0);
-
-  obstacle.add(box1);
-  obstacle.add(box2);
-  obstacle.add(box3);
-
-  obstacle.position.set(x, y, z);
-  return obstacle;
-}
-
-function createObstacle3(x, y, z) {
-  const obstacle = new THREE.Group();
-
-  const box1 = new THREE.Mesh(new THREE.BoxGeometry(15, 24, 10), obstacleMaterial);
-  obstacleBoundingBox = new THREE.Box3().setFromObject(box1);
-  obstaclesBoundingBoxes.push(obstacleBoundingBox);
-
-  const box2 = new THREE.Mesh(new THREE.BoxGeometry(15, 24, 10), obstacleMaterial);
-  obstacleBoundingBox = new THREE.Box3().setFromObject(box2);
-  obstaclesBoundingBoxes.push(obstacleBoundingBox);
-
-  const box3 = new THREE.Mesh(new THREE.BoxGeometry(15, 24, 10), obstacleMaterial);
-  obstacleBoundingBox = new THREE.Box3().setFromObject(box3);
-  obstaclesBoundingBoxes.push(obstacleBoundingBox);
-
-  box1.position.set(15, 6, 0);
-  box2.position.set(0, 6, 0);
-  box3.position.set(-15, -6, 0);
-
-  obstacle.add(box1);
-  obstacle.add(box2);
-  obstacle.add(box3);
-
-  obstacle.position.set(x, y, z);
-  return obstacle;
-}
-
-function addObstaclesToScene(scene) {
-  const randomNum = Math.floor(Math.random() * 3) + 1;
-
-  const z = MIN_Z;
-  const x = 0;
-  const y = 0;
-  let obstacle = null;
-
-  switch(randomNum){
-    case 1:
-      obstacle = createObstacle1(x, y, z);
-      break;
-    case 2:
-      obstacle = createObstacle2(x, y, z);
-      break;
-    case 3:
-      obstacle = createObstacle3(x, y, z);
-      break;
-  }
-  obstacles.push(obstacle);
-  scene.add(obstacle);
-}
 
 function updateGroupBoundingBox(obstacle, index) {
   let i = 0;
   obstacle.traverse(function(child) {
     if (child.isMesh && child.geometry) {
-      child.geometry.computeBoundingBox();
+      //child.geometry.computeBoundingBox();
       obstaclesBoundingBoxes[(index*3) + i].setFromObject(child);
       i++;
     }
   });
-  // for(let j = 0; j < obstacle.children.length; j++){
-  //   obstacle.children[j].geometry.computeBoundingBox();
-  //   obstaclesBoundingBoxes[index*3 + j].setFromObject(obstacle.children[j]);
-  // }
 }
 
 function checkCollision() {
@@ -144,7 +31,7 @@ function checkCollision() {
   }
 }
 
-export function animateObstacles(renderer, camera, scene) {
+export function animateObstacles(renderer, camera, scene, speed) {
   function animate() {
     if(!isPaused){
       for (let i = 0; i < obstacles.length; i++) {
@@ -167,12 +54,12 @@ export function animateObstacles(renderer, camera, scene) {
         }
       }
   
-      if(obstacles[0].position.z == MIN_Z/5){
-        addObstaclesToScene(scene);
+      if(obstacles[0].position.z == -60){
+        createObstacle(scene, MIN_Z);
       }
   
       for (let i = 0; i < obstacles.length; i++) {
-        obstacles[i].position.z += 2; // Adjust the speed as needed
+        obstacles[i].position.z += speed;
       }
   
       renderer.render(scene, camera);
@@ -181,6 +68,6 @@ export function animateObstacles(renderer, camera, scene) {
     requestAnimationFrame(animate);
   }
 
-  addObstaclesToScene(scene);
+  createObstacle(scene, MIN_Z);
   animate();
 }
