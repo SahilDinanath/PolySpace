cf31422df8fc90e5c2c7a78a53e64f59d5c88e1b
Sahil Dinanath <sahildinanath@debian.local>
2023-10-24 16:27:35 +0200

    added world and directional lighting to scene

--- Levels/Level_3.js
+++ Levels/Level_3.js
@@ -2,9 +2,10 @@ import * as bosses from '/Bosses/bosses.js';
 import * as obstacles from '/Obstacles/obstacles.js';
 import * as THREE from 'three';
 import * as player from '/Player/player_exports.js';
-import * as ui from '/UI/ui_exports.js';
-import { Sky } from 'three/examples/jsm/objects/Sky';
+import * as ui from '/UI/ui_exports.js'; //TODO: add import for sky box 
+import * as earth from '../Planets/worldGenerator.js';
 
+let earthTexture = '../Assets/earthTextures/GroundGrassGreen002/GroundGrassGreen002_COL_2K.jpg';
 export function levelThree(scene, renderer, camera) {
 	//sets up lighting 
 	const ambientLighting = new THREE.AmbientLight("white", 6);
@@ -17,14 +18,44 @@ export function levelThree(scene, renderer, camera) {
 	obstacles.animateObstacles(renderer, camera, scene);
 	//uncomment line below to view boss (position currently incorrect and ambient light to bright for texture)
 	initSky(scene, renderer, camera);
+	earth.addSphereToScene(scene,earthTexture);
+	addSunLightingToScene(scene);
 }
+
+export function updateDirectionalLighting(scene){
+
+	const object = scene.getObjectByName("sun");
+	if (object == undefined)
+		return;
+	object.position.y -= 0.01;
+
+
+	}
+//adds directional lighting to scene
+function addSunLightingToScene(scene){
+	const dl = new THREE.DirectionalLight(0xffffff, 2);
+	dl.castShadow = true;
+	dl.position.set(0,100,-300);
+	dl.name = "sun";
+	scene.add(dl);
+}
+
+/* 
+*
+*
+*when I get the folder for backgrounds that khetii created, I'll move the code to that. 
+*code below can be immediatly placed in another file.
+*
+*/
+
+import { Sky } from 'three/examples/jsm/objects/Sky';
 let sky, sun;
 
 let elevation = 2,
 	azimuth = 160,
 	exposure = 0.5;
 
-function initSky(scene, renderer, camera) {
+export function initSky(scene, renderer, camera) {
 
 	// Add Sky
 	sky = new Sky();
@@ -65,7 +96,7 @@ export function updateSkyBox() {
 	if (sky == undefined || sun == undefined)
 		return;
 	const sunsetSpeedDay = 0.0004;
-	const lowestPoint= -10;
+	const lowestPoint = -10;
 	//const sunsetSpeedNight = 0.05;
 	//elevation = elevation > 0 ? elevation - sunsetSpeedDay : elevation - sunsetSpeedNight;
 	elevation = elevation > lowestPoint ? elevation - sunsetSpeedDay : lowestPoint;
